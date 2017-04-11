5bf38430c4468c66289c265eeb4d37319a163074
lovepocky <lovepocky@foxmail.com>
2017-04-11 11:11:56 +0800

    update

---
+++ src/main/scala/GlobalConfig.scala
@@ -0,0 +1,18 @@
+/**
+  * Created by lovepocky on 17/4/11.
+  */
+object GlobalConfig {
+
+  val zkHost = "192.168.1.111"
+  val zkPort = 2181
+
+  val kafkaHost = "192.168.1.111"
+  val kafkaPort = 9092
+
+  val kafkaGroupId = "spark_streaming"
+  val kafkaTopics: Map[String, Int] = Map("test1" -> 1)
+
+  val sparkAppName = "simple_sum"
+  val sparkCheckPoint = "temp_checkpoint"
+
+}
--- src/main/scala/SimpleSum.scala
+++ src/main/scala/SimpleSum.scala
@@ -4,34 +4,36 @@
   */
 object SimpleSum {
 
-    def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {
 
-        import org.apache.log4j.{Level, LogManager}
+    import org.apache.log4j.{Level, LogManager}
 
-        import org.apache.spark._
-        import org.apache.spark.streaming._
-        import org.apache.spark.streaming.kafka.KafkaUtils
+    import org.apache.spark._
+    import org.apache.spark.streaming._
+    import org.apache.spark.streaming.kafka.KafkaUtils
 
-        val conf = new SparkConf().setMaster("local[*]").setAppName("simple_sum")
-        val ssc = new StreamingContext(conf, Seconds(5))
-        ssc.checkpoint("temp")
+    import GlobalConfig._
 
-        val inputDStream = KafkaUtils.createStream(ssc, zkQuorum = "localhost:2181", groupId = "spark_streaming", topics = Map("test" -> 1))
+    val conf = new SparkConf().setMaster("local[*]").setAppName(sparkAppName)
+    val ssc = new StreamingContext(conf, Seconds(5))
+    ssc.checkpoint(sparkCheckPoint)
 
-        val spec =
-            StateSpec.function {
-                (key: String, value: Option[String], state: State[Int]) =>
-                    val originState = state.getOption().getOrElse(0)
-                    val newState = originState + value.map(_.toInt).getOrElse(0)
-                    state.update(newState)
-                    println(s"received number: ${value}, sum: $originState -> $newState")
-                    state.get()
-            }
+    val inputDStream = KafkaUtils.createStream(ssc, zkQuorum = s"$zkHost:$zkPort", groupId = kafkaGroupId, topics = kafkaTopics)
 
-        inputDStream.mapWithState(spec).print(0)
+    val spec =
+      StateSpec.function {
+        (key: String, value: Option[String], state: State[Int]) =>
+          val originState = state.getOption().getOrElse(0)
+          val newState = originState + value.map(_.toInt).getOrElse(0)
+          state.update(newState)
+          println(s"received number: ${value}, sum: $originState -> $newState")
+          state.get()
+      }
 
-        LogManager.getLogger("org").setLevel(Level.ERROR)
-        ssc.start()
-        ssc.awaitTermination()
-    }
+    inputDStream.mapWithState(spec).print(0)
+
+    LogManager.getLogger("org").setLevel(Level.ERROR)
+    ssc.start()
+    ssc.awaitTermination()
+  }
 }
